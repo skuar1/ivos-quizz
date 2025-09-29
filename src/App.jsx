b9ac1d5abb191cb53581829fcd0323827ae6c895
skuar1 <paulohenriquecarato@gmail.com>
2025-09-29 08:42:30 -0300

    archives structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Quiz</h1>
+    <p>i work!</p>
+    </>
+  )
+}
+
+export default App
